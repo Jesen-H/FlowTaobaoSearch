ad99fd2918ee226239f1fc2ef61e9926efe09293
SC-20180227\Administrator <2536950920@qq.com>
2018-09-26 10:46:36 +0800

    仿淘宝搜索

---
+++ app/src/main/java/com/hgeson/flowtaobaosearch/SearchActivity.java
@@ -0,0 +1,106 @@
+package com.hgeson.flowtaobaosearch;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v4.content.SharedPreferencesCompat;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.hgeson.flowtaobaosearch.utils.StorageUtil;
+import com.hgeson.flowtaobaosearch.view.FlowView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Describe：
+ * @Date：2018/9/21
+ * @Author：hgeson
+ */
+
+public class SearchActivity extends Activity implements View.OnClickListener {
+    private EditText editSearch;
+    private TextView tvSearch;
+    private ImageView tvClear;
+    private FlowView flowView;
+    private List<String> data = new ArrayList<>();
+    private LayoutInflater mInflater;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getActionBar() != null){
+            getActionBar().hide();
+        }
+        setContentView(R.layout.activity_search);
+
+        flowView = (FlowView) findViewById(R.id.flow_view);
+        tvSearch = (TextView) findViewById(R.id.tv_search);
+        tvClear = (ImageView) findViewById(R.id.clear_search);
+        editSearch = (EditText) findViewById(R.id.edit_search);
+        mInflater = LayoutInflater.from(this);
+        tvSearch.setOnClickListener(this);
+        tvClear.setOnClickListener(this);
+
+        data = StorageUtil.get(this);
+        setFlow();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.tv_search) {
+            if (editSearch.getText().length() == 0){
+                if (editSearch.getHint().length() != 0){
+                    data.add(editSearch.getHint().toString().trim());
+                }else{
+                    return;
+                }
+            }else {
+                data.add(editSearch.getText().toString().trim());
+            }
+            editSearch.setText("");
+            editSearch.setHint("");
+            flowView.removeAllViews();
+            setFlow();
+        }else{
+            editSearch.setHint("iphone xs");
+            flowView.removeAllViews();
+            data.clear();
+            StorageUtil.remove(this,"",true);
+        }
+    }
+
+    public void setFlow(){
+        for (int i = 0; i < data.size(); i++) {
+            final TextView textView = (TextView) mInflater.inflate(R.layout.item_tv, flowView, false);
+            textView.setText(data.get(i));
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //自定义点击事件，这里我写出点击item执行删除操作
+                    for (int j = 0; j < data.size(); j++) {
+                        if (data.get(j).equals(textView.getText().toString())) {
+                            data.remove(j);
+                            flowView.removeView(v);
+                            StorageUtil.remove(SearchActivity.this,String.valueOf(j),false);
+                            break;
+                        }
+                    }
+                }
+            });
+            flowView.addView(textView);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        StorageUtil.share(this,data);
+    }
+}
---
+++ app/src/main/java/com/hgeson/flowtaobaosearch/utils/StorageUtil.java
@@ -0,0 +1,52 @@
+package com.hgeson.flowtaobaosearch.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v4.content.SharedPreferencesCompat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.content.Context.MODE_PRIVATE;
+
+/**
+ * @Describe：存储搜索对象
+ * @Date：2018/9/21
+ * @Author：hgeson
+ */
+
+public class StorageUtil {
+    private static final String FILE_NAME = "FILE_NAME";
+
+    public static List<String> get(Context context){
+        List<String> list = new ArrayList<>();
+        SharedPreferences sp = context.getSharedPreferences(FILE_NAME,MODE_PRIVATE);
+        int datasize = sp.getInt("datasize", 0);
+        for (int i = 0; i < datasize; i++) {
+            list.add(sp.getString(i + "",""));
+        }
+        return list;
+    }
+
+    public static void share(Context context,List<String> data){
+        SharedPreferences sp = context.getSharedPreferences(FILE_NAME,MODE_PRIVATE);
+        SharedPreferences.Editor edit = sp.edit();
+        for (int i = 0; i < data.size(); i++) {
+            edit.putString(i + "",data.get(i));
+        }
+        edit.putInt("datasize",data.size());
+        edit.commit();
+    }
+
+    public static void remove(Context context,String key,boolean isClear){
+        SharedPreferences sp = context.getSharedPreferences(FILE_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor edit = sp.edit();
+        if (isClear){
+            edit.clear();
+        }else{
+            edit.remove(key);
+        }
+        SharedPreferencesCompat.EditorCompat.getInstance().apply(edit);
+    }
+}
